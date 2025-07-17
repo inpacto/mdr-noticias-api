861d9f29520800d12ada254ae4bb288282e9a946
Danilo Soares <webdesign@inpacto.co>
2025-07-17 15:17:45 -0300

    Ajuste no astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from 'astro/config';
-import icon from 'astro-icon';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
 import node from '@astrojs/node';
 
 export default defineConfig({
-    integrations: [tailwind(), icon(), react()],
     output: 'server',
     adapter: node({ mode: 'standalone' }),
     server: {
